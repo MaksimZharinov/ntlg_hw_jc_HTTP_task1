7bac65ae266254064e4277368148dfaed2da3316
Maksim Zharinov <minimum.cold1991@gmail.com>
2024-05-02 01:30:58 +0700

    create list of Posts in Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -5,8 +7,12 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
+
+    public static ObjectMapper mapper = new ObjectMapper();
+
     public static void main(String[] args) throws IOException {
         CloseableHttpClient httpClient = HttpClientBuilder.create()
                 .setDefaultRequestConfig(RequestConfig.custom()
@@ -19,5 +25,8 @@ public class Main {
         HttpGet request = new HttpGet("https://raw.githubusercontent.com" +
                 "/netology-code/jd-homeworks/master/http/task1/cats");
         CloseableHttpResponse response = httpClient.execute(request);
+
+        List<Rspns> rspnsList = mapper.readValue(response.getEntity().getContent(),
+                new TypeReference<List<Rspns>>() {});
     }
 }
