787d0fc8bb27e680bd0c51c64bc7e0a4330faa1e
Maksim Zharinov <minimum.cold1991@gmail.com>
2024-05-01 18:08:02 +0700

    created Object httpClient

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,17 @@
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build();
+    }
 }
